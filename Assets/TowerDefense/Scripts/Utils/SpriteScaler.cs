2e0553bf39543a79f9e4f567dec819cc7ee24efd
Kynes <kamilpirog98@gmail.com>
2024-04-23 21:12:59 +0200

    feat: handle safe area in sprite scaler

--- Assets/TowerDefense/Scripts/Utils/SpriteScaler.cs
+++ Assets/TowerDefense/Scripts/Utils/SpriteScaler.cs
@@ -5,6 +5,8 @@ namespace TowerDefense.Scripts.Utils
     [ExecuteAlways, RequireComponent(typeof(SpriteRenderer))]
     public class SpriteScaler : MonoBehaviour
     {
+        [SerializeField] private bool includeSafeArea;
+        
         private SpriteRenderer _renderer;
 
         private void Awake()
@@ -16,7 +18,7 @@ namespace TowerDefense.Scripts.Utils
         {
             Scale();
         }
-
+        
         private void Scale()
         {
             if (_renderer == null)
@@ -27,12 +29,18 @@ namespace TowerDefense.Scripts.Utils
 
             var cameraHeight = mainCamera.orthographicSize * 2f;
             var cameraWidth = cameraHeight * mainCamera.aspect;
+            
+            if (includeSafeArea)
+            {
+                var safeArea = Screen.safeArea;
+                cameraWidth *= safeArea.width / Screen.width;
+                cameraHeight *= safeArea.height / Screen.height;
+                
+                Vector2 safeAreaCenter =mainCamera.ScreenToWorldPoint(safeArea.center);
+                transform.position = safeAreaCenter;
+            }
 
-            var scale = transform.localScale;
-            scale.x = cameraWidth / spriteSize.x;
-            scale.y = cameraHeight / spriteSize.y;
-
-            transform.localScale = scale;
+            transform.localScale = new Vector3(cameraWidth / spriteSize.x, cameraHeight / spriteSize.y);
         }
     }
 }
