7d486f8116a02ea075db7724ea24578e0ed90ab1
Kynes <kamilpirog98@gmail.com>
2024-05-04 17:31:06 +0200

    chore: import zenject signals

---
+++ Assets/TowerDefense/AI/Scripts/EnemyAnimationHandler.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace TowerDefense.AI.Scripts
+{
+    public class EnemyAnimationHandler
+    {
+        private readonly Animator _animator;
+
+        public EnemyAnimationHandler(Animator animator)
+        {
+            _animator = animator;
+        }
+    }
+}
