0975dcdd611a72d33b4474b204fb0b47fafd451a
Kynes <kamilpirog98@gmail.com>
2024-06-17 16:06:01 +0200

    chore: add more towers scriptable objects datas

--- Assets/TowerDefense/Scripts/Towers/TowerData.cs
+++ Assets/TowerDefense/Scripts/Towers/TowerData.cs
@@ -6,11 +6,21 @@ namespace TowerDefense.Scripts.Towers
     public class TowerData : ScriptableObject
     {
         [field: SerializeField] public string Name { get; private set; }
-        [field: SerializeField] public Sprite TowerSprite { get; private set; }
+        [field: SerializeField] public int RequiredStars { get; private set; }
         [field: SerializeField] public Sprite Icon { get; private set; }
         [field: SerializeField] public TowerFacade Prefab { get; private set; }
         [field: SerializeField] public TowerData Upgrade { get; private set; }
 
         public bool CanUpgrade => Upgrade != null;
+
+        public bool IsUnlocked()
+        {
+            if (RequiredStars == 0)
+                return true;
+
+            // TODO: Load Player's stars and compare them with RequiredStars
+            
+            return false;
+        }
     }
 }
